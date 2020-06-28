7271661dbb900709e398262d7426569babda147d
bbachi <bhargavbachina@yahoo.com>
2020-06-28 15:54:42 -0500

    initial commit

---
+++ src/main/resources/static/precache-manifest.a34f9c9675b24e70218c2bd79e4eab69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14f493d0b1c88b5a5311687a8fce71d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5009b8f8302e304bd12f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4eb99f91ed69b5fa8514",
+    "url": "/static/css/main.14ff7981.chunk.css"
+  },
+  {
+    "revision": "5009b8f8302e304bd12f",
+    "url": "/static/js/2.28dc267f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.28dc267f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb99f91ed69b5fa8514",
+    "url": "/static/js/main.d3962698.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
